b4fc0df5c7da4b35763e7087d27bb954d43f63d3
reshm <reshm@DESKTOP-F10JV1T>
2024-09-04 01:31:30 +0530

    Solution TestCase 6

--- fizzbuzzkata/src/main/java/com/katalearn/fizzbuzzkata/FizzBuzz.java
+++ fizzbuzzkata/src/main/java/com/katalearn/fizzbuzzkata/FizzBuzz.java
@@ -4,9 +4,14 @@ public class FizzBuzz {
 
 	public String getFizzBuzzValue(int num) {
 
-		if(num==0) return String.valueOf(num);
-		if(num%3 == 0) return "Fizz";
-		if(num%5 == 0) return "Buzz";
+		if (num == 0)
+			return String.valueOf(num);
+		if (num % 3 == 0 && num % 5 == 0)
+			return "FizzBuzz";
+		if (num % 3 == 0)
+			return "Fizz";
+		if (num % 5 == 0)
+			return "Buzz";
 		return String.valueOf(num);
 	}
 
