e1cb9589ffde1a3f056b5e93bf0ff42538eda4b9
reshm <reshm@DESKTOP-F10JV1T>
2024-09-04 01:39:26 +0530

    Refactor 2

--- fizzbuzzkata/src/main/java/com/katalearn/fizzbuzzkata/FizzBuzz.java
+++ fizzbuzzkata/src/main/java/com/katalearn/fizzbuzzkata/FizzBuzz.java
@@ -4,19 +4,20 @@ public class FizzBuzz {
 
 	private static final String FIZZ = "Fizz";
 	private static final String BUZZ = "Buzz";
-	private static final String FIZZBUZZ = "FizzBuzz";
-	
+
 	public String getFizzBuzzValue(int num) {
+
+		StringBuilder result = new StringBuilder();
 
 		if (num == 0)
 			return String.valueOf(num);
-		if (isMultipleOf3(num) && isMultipleOf5(num))
-			return FIZZBUZZ;
+
 		if (isMultipleOf3(num))
-			return FIZZ;
+			result.append(FIZZ);
 		if (isMultipleOf5(num))
-			return BUZZ;
-		return String.valueOf(num);
+			result.append(BUZZ);
+
+		return result.length() > 0 ? result.toString() : String.valueOf(num);
 	}
 
 	private boolean isMultipleOf5(int num) {
